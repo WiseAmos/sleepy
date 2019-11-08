74846cfe536478ded23ba9c0dcee999db86ca025
Amos <53104046+WiseAmos@users.noreply.github.com>
2019-11-08 22:51:07 +0800

    Initial Commit

---
+++ sleepy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sleepy
+//
+//  Created by Amos Goh En Jie on 8/11/19.
+//  Copyright © 2019 Amos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
